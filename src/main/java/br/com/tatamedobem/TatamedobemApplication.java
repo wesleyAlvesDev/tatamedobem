6ada6f94cad9d512efeb32c3a75df81ce6946756
Wesley Alves <wesleydrrr@gmail.com>
2026-04-16 20:26:31 -0300

    Initialize Spring Boot application with Maven
    Bootstrap the project structure using Spring Boot 3.5.13 and Java 21. This includes the Maven wrapper, basic dependencies (Web, Data JPA, PostgreSQL, Lombok), and the main application entry point.

---
+++ src/main/java/br/com/tatamedobem/TatamedobemApplication.java
@@ -0,0 +1,13 @@
+package br.com.tatamedobem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TatamedobemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TatamedobemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tatamedobem/TatamedobemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tatamedobem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TatamedobemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
